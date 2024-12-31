f772769cf2d0b61015a2b693d174a1878ebbc583
Luciano Ferreira <luciano2607ferreira@gmail.com>
2024-12-31 16:18:51 +0000

    ja da para remover a peça do seu sitio inicial

--- src/move.pl
+++ src/move.pl
@@ -1,21 +1,22 @@
-move_put_piece(CurrentBoard, Player_Piece, Row-Column-Color, NewBoard) :- 
-    move_update_board(CurrentBoard, Player_Piece, 0-0, TempBoard).
+move_put_piece(CurrentBoard, Player_Piece, NewBoard) :- 
+    move_update_board(CurrentBoard, Player_Piece, 0-0, NewBoard).
 
 move_update_board([], _, _, []).
 move_update_board([Row | Rest], Player_Piece, TargetValue, [UpdatedRow | UpdatedRest]) :-
     move_update_row(Player_Piece, TargetValue, Row, UpdatedRow), 
     move_update_board(Rest, Player_Piece, TargetValue, UpdatedRest).
 
-move_update_row(_, _, [], []). % Base case: empty row
-move_update_row(Player-Piece, TargetValue, [tile(_, _, [( _, Player-Piece), ( _, _), ( _, _), ( _, _)]) | Rest], [tile(_, _, [( _, TargetValue), ( _, _), ( _, _), ( _, _)])|Rest]).
-move_update_row(Player-Piece, TargetValue, [tile(_, _, [( _, _), ( _, Player-Piece), ( _, _), ( _, _)]) | Rest], [tile(_, _, [( _, _), ( _, TargetValue), ( _, _), ( _, _)])|Rest]).
-move_update_row(Player-Piece, TargetValue, [tile(_, _, [( _, _), ( _, _), ( _, Player-Piece), ( _, _)]) | Rest], [tile(_, _, [( _, _), ( _, _), ( _, TargetValue), ( _, _)])|Rest]).
-move_update_row(Player-Piece, TargetValue, [tile(_, _, [( _, _), ( _, _), ( _, _), ( _, Player-Piece)]) | Rest], [tile(_, _, [( _, _), ( _, _), ( _, _), ( _, TargetValue)])|Rest]).
-move_update_row(Player-Piece, TargetValue, [tile(X, Y, Tokens) | Rest], [tile(X, Y, Tokens) | UpdatedRest]) :-
-    \+ member((_, Player-Piece), Tokens),
+move_update_row(_, _, [], []) :- !. % Base case: empty row
+move_update_row(Player-Piece, TargetValue, [tile(A, B, [(C, Player-Piece), D, E, F]) | Rest], [tile(A, B, [(C, TargetValue), D, E, F]) | UpdatedRest]) :-
     move_update_row(Player-Piece, TargetValue, Rest, UpdatedRest).
-
-
+move_update_row(Player-Piece, TargetValue, [tile(A, B, [C, (D, Player-Piece), E, F]) | Rest], [tile(A, B, [C, (D, TargetValue), E, F]) | UpdatedRest]) :-
+    move_update_row(Player-Piece, TargetValue, Rest, UpdatedRest).
+move_update_row(Player-Piece, TargetValue, [tile(A, B, [C, D, (E, Player-Piece), F]) | Rest], [tile(A, B, [C, D, (E, TargetValue), F]) | UpdatedRest]) :-
+    move_update_row(Player-Piece, TargetValue, Rest, UpdatedRest).
+move_update_row(Player-Piece, TargetValue, [tile(A, B, [C, D, E, (F, Player-Piece)]) | Rest], [tile(A, B, [C, D, E, (F, TargetValue)]) | UpdatedRest]) :-
+    move_update_row(Player-Piece, TargetValue, Rest, UpdatedRest).
+move_update_row(Player-Piece, TargetValue, [tile(A, B, C) | Rest], [tile(A, B, C) | UpdatedRest]) :-
+    move_update_row(Player-Piece, TargetValue, Rest, UpdatedRest).
 
 board([
     [tile(1, 1, [(o-orange, 1-2), (g-black, 0-0), (b-blue, 0-0), (' '-black, 0-1)]),
@@ -23,3 +24,6 @@ board([
     [tile(2, 1, [(o-orange, 0-0), (g-black, 0-0), (b-blue, 0-0), (' '-black, 0-1)]),
      tile(2, 2, [(o-orange, 0-0), (g-black, 0-0), (b-blue, 0-0), (' '-black, 0-1)])]
 ]).
+
+% Example query to test the code
+% board(Board), move_put_piece(Board, 1-2, NewBoard).
